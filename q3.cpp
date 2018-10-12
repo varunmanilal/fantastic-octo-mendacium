04d5b73a77206c2fd1b79fc72a2c8eaf9cd35a04
Varun <42139581+varunmanilal@users.noreply.github.com>
2018-10-12 23:21:03 +0530

    Update q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -1,5 +1,7 @@
+//the library file
 #include<iostream>
 using namespace std;
+//the function which will print all the odd numbers upto any number you type
 int qw(int a,int b)
 {
  if (a>b) {
@@ -9,6 +11,7 @@ int qw(int a,int b)
 	qw(a,b);
 	return 0;}
 }
+//the main function
 int main(){
 int d;
 cout<<"Type the number upto which you want all odd numbers printed"<<endl;
